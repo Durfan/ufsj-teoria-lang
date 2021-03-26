c291e12e69eab1d634c1f1ad558acf7ca53f5310
Durfan <cecilio@aluno.ufsj.edu.br>
2021-03-26 03:14:30 -0300

    beta

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import argparse
-import sys, time
+import sys, timeit
 
-start_time = time.time()
+start_time = timeit.default_timer()
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,7 +25,7 @@ dfa = {
     5:{'p':4}
 }
 
-def sread(s,output=None):
+def sread(s,out=None):
     initial = 0
     accepting = {5}
     indefinition = False
@@ -44,43 +44,56 @@ def sread(s,output=None):
     if indefinition:
         result = '-I : {}'.format(cursor)
         print(bcolors.FAIL + result + bcolors.ENDC)
-        output.write(result + '\n') if output else None
+        out.write(result + '\n') if out else None
     elif q not in accepting:
         result = '-{} : {}'.format(q,s)
         print(bcolors.FAIL + result + bcolors.ENDC)
-        output.write(result + '\n') if output else None
+        out.write(result + '\n') if out else None
     else:
         result = '+A : {}'.format(s)
         print(bcolors.OKGREEN + result + bcolors.ENDC)
-        output.write(result + '\n') if output else None
+        out.write(result + '\n') if out else None
 
     return state in accepting
 
 if __name__ == '__main__':
-    print('UFSJ/T.Linguagens TP1')
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s','--string')
-    parser.add_argument('-f','--file', type=argparse.FileType('r', encoding='UTF-8'))
-    parser.add_argument('-o','--output', type=argparse.FileType('w', encoding='UTF-8'))
+    parser = argparse.ArgumentParser(description='UFSJ/T.Linguagens TP1')
+    parser.add_argument('-s','--str', help='string de entrada')
+    parser.add_argument('-f','--file',
+        type=argparse.FileType('r', encoding='UTF-8'),
+        help='arquivo de entrada')
+    parser.add_argument('-o','--out',
+        type=argparse.FileType('w', encoding='UTF-8'),
+        help='arquivo de saida')
+    parser.add_argument('-l','--legend',
+        action="store_true",
+        help='mostrar leganda')
     args = parser.parse_args()
 
-    if not (args.string or args.file or args.output):
-        parser.error('Nenhuma acao requisitada')
+    if not (args.str or args.file or args.out):
+        parser.print_help()
+        sys.exit(1)
 
-    print('ER = 5(7p+8a)\u207A7\n')
+    print('ER = 5(7p+8a)\u207A7')
 
     if args.file:
         strings = args.file.read().splitlines()
         for s in strings:
-            sread(s,args.output) if args.output else sread(s)
+            sread(s,args.out) if args.out else sread(s)
         args.file.close()
 
-    if args.string:
-        sread(args.string)
+    if args.str:
+        sread(args.str)
 
-    if args.output:
-        args.output.close()
+    if args.out:
+        args.out.close()
 
-    print('\n+A Aceito -I Indefinido -# Nao Final')
-    print('{}s'.format(time.time() - start_time))
+    if args.legend:
+        stop_time = timeit.default_timer()
+        execution_time = round(stop_time - start_time,4)
+        print('+A : Aceito')
+        print('-I : Rejeitado (Indefinido)')
+        print('-# : Rejeitado (Estado nao final)')
+        print(' t : {}s'.format(execution_time))
+
     sys.exit(0)
